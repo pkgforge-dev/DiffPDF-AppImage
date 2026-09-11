c72e9522c7411bbf31059d0655a58ba6350eedc7
Link4Electronics <link4electronics@gmail.com>
2026-09-10 23:13:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    diffpdf \
-    qt5-wayland
+pacman -Syu --noconfirm diffpdf qt5-wayland
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
